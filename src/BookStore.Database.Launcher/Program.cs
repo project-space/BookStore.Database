7ece17b2cc7c071b07d6afe8d006a345677a0357
Elladiel <nastyazubastya1999@mail.ru>
2017-10-04 00:44:29 +0300

    Sample

--- src/BookStore.Database.Launcher/Program.cs
+++ src/BookStore.Database.Launcher/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace BookStore.Database.Launcher
+﻿using DbUp;
+using System;
+using System.Reflection;
+
+namespace BookStore.Database.Launcher
 {
     class Program
     {
-        static void Main(string[] args)
+        static string _identityDatabaseConnectionString = 
+            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BookStore.IdentityServer;Integrated Security=True;";
+
+        static int Main(string[] args)
         {
+            EnsureDatabase.For.SqlDatabase(_identityDatabaseConnectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(_identityDatabaseConnectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            Console.ReadKey();
+            return 0;
         }
     }
 }
---
+++ src/BookStore.Database.Launcher/Scripts/00000 - create account table.sql
@@ -0,0 +1,5 @@
+﻿create table Account (
+	Id int not null identity(1,1) primary key,
+	Login varchar(64) not null,
+	Password varchar(32) not null
+)
