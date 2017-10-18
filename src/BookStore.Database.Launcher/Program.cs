f896d07d9b4091294988551f6c0ef4a2f9d8548c
Elladiel <nastyazubastya1999@mail.ru>
2017-10-18 08:52:46 +0300

    added database migrations

--- src/BookStore.Database.Launcher/Program.cs
+++ src/BookStore.Database.Launcher/Program.cs
@@ -6,17 +6,29 @@ namespace BookStore.Database.Launcher
 {
     class Program
     {
-        static string _identityDatabaseConnectionString = 
-            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BookStore.IdentityServer;Integrated Security=True;";
+        static string identityConnectionString =   @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BookStore.IdentityServer;Integrated Security=True;";
+        static string bookConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BookStore.Book;Integrated Security=True;";
+        static string basketConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BookStore.Basket;Integrated Security=True;";
 
-        static int Main(string[] args)
+        static Assembly identityServerAssembly = Assembly.Load("BookStore.Database.IdentityServer");
+        static Assembly bookAssembly = Assembly.Load("BookStore.Database.Book");
+        static Assembly basketAssembly = Assembly.Load("BookStore.Database.Basket");
+
+        static void Main(string[] args)
         {
-            EnsureDatabase.For.SqlDatabase(_identityDatabaseConnectionString);
+            RunMigrations(identityConnectionString, identityServerAssembly);
+            RunMigrations(bookConnectionString, bookAssembly);
+            RunMigrations(basketConnectionString, basketAssembly);
+        }
+
+        static void RunMigrations (string connectionString, Assembly assemblyWithScripts)
+        {
+            EnsureDatabase.For.SqlDatabase(connectionString);
 
             var upgrader =
                 DeployChanges.To
-                    .SqlDatabase(_identityDatabaseConnectionString)
-                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(assemblyWithScripts)
                     .LogToConsole()
                     .Build();
 
@@ -28,16 +40,15 @@ namespace BookStore.Database.Launcher
                 Console.WriteLine(result.Error);
                 Console.ResetColor();
 #if DEBUG
-                Console.ReadLine();
+                Console.WriteLine("Press any key to continue ...");
+                Console.ReadKey();
 #endif
-                return -1;
             }
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Success!");
-            Console.ResetColor();
+#if DEBUG
+            Console.WriteLine("Press any key to continue ...");
             Console.ReadKey();
-            return 0;
+#endif
         }
     }
 }
