9cbee1a0c0baf84b6ac2a39b6f8c72db25c6b73e
Elladiel <nastyazubastya1999@mail.ru>
2017-11-04 01:02:01 +0300

    fixed encoding in table Book

---
+++ src/BookStore.Database.Book/Scripts/2017/00002 - alter book table.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE Book ALTER COLUMN Title nvarchar(32) not null
+ALTER TABLE Book ALTER COLUMN Author nvarchar(32) not null
+ALTER TABLE Book ALTER COLUMN Description nvarchar(max) not null
