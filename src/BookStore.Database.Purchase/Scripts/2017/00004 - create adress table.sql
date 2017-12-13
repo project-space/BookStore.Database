4311a893942ee7e1b804e39ba63d6abc98c218ff
Elladiel <nastyazubastya1999@mail.ru>
2017-12-14 00:24:08 +0300

    created adress table

---
+++ src/BookStore.Database.Purchase/Scripts/2017/00004 - create adress table.sql
@@ -0,0 +1,10 @@
+﻿create table Adress(
+	Id int not null identity(1,1) primary key,
+	Postcode varchar(6) not null,
+	Country nvarchar(25) not null,
+	City nvarchar(25) not null,
+	Street nvarchar(30) not null,
+	House nvarchar(5) not null,
+	Apartment varchar(5) not null,
+	PhoneNumber varchar(15)
+)
