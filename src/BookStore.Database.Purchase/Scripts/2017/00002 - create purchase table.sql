e53bfe53161b253aa5c1b2e0ff2e2f302014f621
Elladiel <nastyazubastya1999@mail.ru>
2017-12-14 00:23:47 +0300

    created purchase and purchase item tables

---
+++ src/BookStore.Database.Purchase/Scripts/2017/00002 - create purchase table.sql
@@ -0,0 +1,6 @@
+﻿create table Purchase(
+	Id int not null identity(1,1) primary key,
+	AccountId int,
+	GuestId int,
+	PurchaseDetailsId int not null
+)
---
+++ src/BookStore.Database.Purchase/Scripts/2017/00003 - create purchase item table.sql
@@ -0,0 +1,5 @@
+﻿create table PurchaseItem(
+	Id int not null identity(1,1) primary key,
+	PurchaseId int not null,
+	BookId int not null
+)
