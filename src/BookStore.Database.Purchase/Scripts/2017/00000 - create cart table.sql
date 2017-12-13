9be9283970b216034418bcefe532d2173951ca64
Elladiel <nastyazubastya1999@mail.ru>
2017-12-13 23:22:56 +0300

    created cart and cartItem tables

---
+++ src/BookStore.Database.Purchase/Scripts/2017/00000 - create cart table.sql
@@ -0,0 +1,5 @@
+﻿create table Cart (
+	Id int Identity(1,1) not null primary key,
+	AccountId int not null,
+	GuestId int not null
+)
---
+++ src/BookStore.Database.Purchase/Scripts/2017/00001 - create cart item table.sql
@@ -0,0 +1,5 @@
+﻿create table CartItem (
+	Id int not null identity(1,1) primary key,
+	CartId int not null,
+	BookId int not null
+)
