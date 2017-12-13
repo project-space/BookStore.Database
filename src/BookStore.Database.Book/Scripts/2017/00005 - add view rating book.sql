5287774bc161152a1b666f2331d9dc8e3fe56003
Elladiel <nastyazubastya1999@mail.ru>
2017-12-13 22:57:51 +0300

    added view rating book

---
+++ src/BookStore.Database.Book/Scripts/2017/00005 - add view rating book.sql
@@ -0,0 +1,5 @@
+﻿create view RatingBook
+as
+select b.*, coalesce(sum(g.Score),0)
+ as Rating from Book as b
+left join Grade as g on b.Id = g.BookId group by b.Id, b.Title, b.Author, b.Price, b.Description, b.ReleaseDate, b.CoverUrl
